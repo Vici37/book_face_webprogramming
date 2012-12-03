0b52de53873a40e6534ec4d9dbea04d653d4781f
Troy A. Sornson <troyasornson37@gmail.com>
2012-12-02 21:54:24 -0700

    Added status and friends to bookface.

---
+++ status_box.php
@@ -0,0 +1,20 @@
+<?
+	if(isset($_REQUEST['update_status'])) {
+		if ($_REQUEST['status_update'] != "") {
+			include_once("./db_connect.inc");
+			session_start();
+			$datetime = strftime("%F %T");
+			$query = "INSERT INTO statuses (status_id, user_id, status, time) VALUES (NULL, '".$_SESSION['user_id']."', '".addslashes($_REQUEST['status_update'])."', '".$datetime."')";
+			$db->query($query);
+		}
+		header('location:./index.php?home');
+	} 
+?>
+<div id="status_update">
+	<form action="status_box.php" method="post" accept-charset="utf-8">
+		<input type="hidden" name="update_status" value="1">
+		<label>Status</label><br/>
+		<textarea name="status_update" rows="8" cols="40"></textarea>
+	<input type="submit" value="Submit">
+	</form>
+</div>

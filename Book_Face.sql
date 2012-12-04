297ce09502b8fad3fb4a884d9a7f094f1d257b32
Tomas Morris <tomas.morris13@gmail.com>
2012-12-03 21:03:47 -0700

    updated sql file to create only if doesn't exist

--- Book_Face.sql
+++ Book_Face.sql
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 --
 -- Database: `Book_Face`
 --
-CREATE DATABASE `Book_Face` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+CREATE DATABASE IF NOT EXISTS `Book_Face` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `Book_Face`;
 
 -- --------------------------------------------------------

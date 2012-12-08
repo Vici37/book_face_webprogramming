38d4cf3422f095458b11d9864a7e80e070cf7dc1
Brianna Fidder <bfidder@mines.edu>
2012-12-07 23:35:38 -0700

    friend requests working on members page

--- add_friend.php
+++ add_friend.php
@@ -4,7 +4,7 @@
 	$query = "SELECT * FROM friends_requests WHERE user_id='".$_SESSION['user_id']."' AND requesting_user_id='".$_REQUEST['id']."'";
 	$result = $db->query($query);
 	if($result->num_rows == 0) {
-		$query = "INSERT INTO friends_requests (user_id, requesting_user_id) VALUES (".$_SESSION['user_id'].", ".$_REQUEST['id'].")";
+		$query = "INSERT INTO friend_requests (user_id, requesting_user_id) VALUES (".$_SESSION['user_id'].", ".$_REQUEST['id'].")";
 		$db->query($query);
 		header('location:./index.php?friends');
 	} else {

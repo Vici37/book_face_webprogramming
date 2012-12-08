53f54a034ed2c2f012dd0e6afdbfaf042a2a29ce
Brianna Fidder <bfidder@mines.edu>
2012-12-07 21:36:10 -0700

    updating friend requests

--- add_friend.php
+++ add_friend.php
@@ -1,10 +1,10 @@
 <?
 	require_once('./db_connect.inc');
 	session_start();
-	$query = "SELECT * FROM friends_relations WHERE user_id='".$_SESSION['user_id']."' AND friend_user_id='".$_REQUEST['id']."'";
+	$query = "SELECT * FROM friends_requests WHERE user_id='".$_SESSION['user_id']."' AND requesting_user_id='".$_REQUEST['id']."'";
 	$result = $db->query($query);
 	if($result->num_rows == 0) {
-		$query = "INSERT INTO friends_relations (user_id, friend_user_id) VALUES (".$_SESSION['user_id'].", ".$_REQUEST['id'].")";
+		$query = "INSERT INTO friends_requests (user_id, requesting_user_id) VALUES (".$_SESSION['user_id'].", ".$_REQUEST['id'].")";
 		$db->query($query);
 		header('location:./index.php?friends');
 	} else {

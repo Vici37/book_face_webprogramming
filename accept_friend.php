62dfe20a0a9c20b2c270e525619375c3611615fa
Brianna Fidder <bfidder@mines.edu>
2012-12-09 17:52:39 -0700

    Finished Friend Requests functionality and delete friends functionality

--- accept_friend.php
+++ accept_friend.php
@@ -6,7 +6,9 @@
 	if($result->num_rows == 0) {
 		$query = "INSERT INTO friends_relations (user_id, friend_user_id) VALUES (".$_SESSION['user_id'].", ".$_REQUEST['id'].")";
 		$db->query($query);
-		$query = "DELETE * FROM friends_requests WHERE user_id='".$_SESSION['user_id']."' AND requesting_user_id='".$_REQUEST['id']."'";
+		$query = "INSERT INTO friends_relations (user_id, friend_user_id) VALUES (".$_REQUEST['id'].", ".$_SESSION['user_id'].")";
+		$db->query($query);
+		$query = "DELETE FROM friend_requests WHERE user_id='".$_SESSION['user_id']."' AND requesting_user_id='".$_REQUEST['id']."'";
 		$db->query($query);
 		header('location:./index.php?friends');
 	} else {
--- add_friend.php
+++ add_friend.php
@@ -3,8 +3,10 @@
 	session_start();
 	$query = "SELECT * FROM friends_requests WHERE user_id='".$_SESSION['user_id']."' AND requesting_user_id='".$_REQUEST['id']."'";
 	$result = $db->query($query);
-	if($result->num_rows == 0) {
-		$query = "INSERT INTO friend_requests (user_id, requesting_user_id) VALUES (".$_SESSION['user_id'].", ".$_REQUEST['id'].")";
+	$query = "SELECT * FROM friend_requests WHERE user_id='".$_REQUEST['id']."' AND requesting_user_id='".$_SESSION['user_id']."'";
+	$check = $db->query($query);
+	if(($result->num_rows == 0) and ($check->num_rows == 0)) {
+		$query = "INSERT INTO friend_requests (user_id, requesting_user_id) VALUES (".$_REQUEST['id'].", ".$_SESSION['user_id'].")";
 		$db->query($query);
 		header('location:./index.php?friends');
 	} else {
